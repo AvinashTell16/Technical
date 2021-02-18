91a49a0be390c4fc1c6180061bf8294e6ceb3a63
AvinashTell16 <79193449+AvinashTell16@users.noreply.github.com>
2021-02-18 16:50:37 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'Github documentation by Avinash'
+copyright = '2020, Avinash'
+author = 'Tellakula Avinash'
 
 # The full version, including alpha/beta/rc tags
 release = ''
